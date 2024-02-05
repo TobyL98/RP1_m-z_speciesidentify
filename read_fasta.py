865afcca54c1124be596a225bcf3171d7dd9a122
TobyL98 <158182593+TobyL98@users.noreply.github.com>
2024-02-05 11:56:27 +0000

    Adding initial files

---
+++ read_fasta.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+def readFastaFile(fileName):
+    fileObj = open(fileName, 'r')
+    sequences = {}   #  a dict, to contain all our sequences ...
+    names = []       #  ... and a list to contain their names
+    count = 0 
+    for line in fileObj:
+        if line.startswith('>'):   # Ah ha ! a new sequence?
+            header = line[1:].rstrip('\n')  # remove newline, ignore '>'
+            name_list = header.split()[-2:]       # take first and second of line, name of organism
+            name = ' '.join(name_list)
+            name = name.strip("[]")
+
+            if name in names:
+                count +=1 
+                name = name + " " + str(count)
+            names.append(name)
+            sequences[name] = ''
+        else:
+            sequences[name] += line.rstrip('\n')  # concat to growing dict value
+    fileObj.close()
+    return (sequences)
+
+file = 'sequence.fasta'
+output_seq = readFastaFile(file)
+print(output_seq)
